12842a591e88dd3cad890e8af95e80fc239f5343
mariaarcas06-blip <mariaarcas06@gmail.com>
2026-06-30 15:54:24 -0300

    primer commit

---
+++ practicas python/python practica.py
@@ -0,0 +1,60 @@
+horas = 10
+clases = 60
+txt = "el contenido de este curso va a duarar {1} horas y tendra {0} clases "
+
+print (txt. format (clases,horas)) 
+
+
+txt = "este texto esta todo en minuscula" 
+
+print (txt.title())
+
+
+txt ="este texto esta todo en minuscula "
+word = "centrado"
+
+#print(txt.title())
+
+print(word.center(20))
+
+txt = "la manzana es mi fruta favorita ¿tienes alguna manzana?"
+word = "centrado"
+print (txt.count("manzana"))
+ 
+#print(word.center(20))
+
+
+a = 5
+b = 4
+c = a+ b 
+
+print (c) 
+
+c = a * b
+
+print (c)
+print (type(c))
+
+
+# / para dividir
+# //  para dividir entero ( floor divicion)
+# % resto o modulo ( modulus)
+ 
+a = 5
+b = 4
+c = a // b
+d = a % b
+
+print (c) ## division de enteros
+print (d) ## el resto
+
+
+#operadores de asignacion:
+a = "este es un texto"
+b = 123
+
+x = 10
+
+x = 15
+
+print (x) 
